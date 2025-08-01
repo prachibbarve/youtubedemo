66d9600245355208945d12ae5a756429f548aeb5
Prachi Joshi <prachibbarve@gmail.com>
2025-08-01 23:17:04 +0530

    Update constants.jsx

--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -1,5 +1,5 @@
-export const YOUTUBE_API_KEY = "AIzaSyArI6IJndmRnV7Sgsk2rnC-FFTFdBg9Xjk";
-//export const YOUTUBE_API_KEY = "AIzaSyBeo4NGA__U6Xxy-aBE6yFm19pgq8TY-TM";
+export const YOUTUBE_API_KEY = "";
+
 export const YOUTUBE_API =
   "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=40&regionCode=IN&key=" +
   YOUTUBE_API_KEY;
